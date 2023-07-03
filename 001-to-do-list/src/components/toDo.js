3aad991176eb274cb4b30009f9ec39cf3c287a5d
Ismael <isma@isma.es>
2023-07-03 12:27:49 +0200

    add Delete function and TailWindCss

--- 001-to-do-list/src/components/toDo.js
+++ 001-to-do-list/src/components/toDo.js
@@ -1,56 +1,56 @@
 import { useState } from "react";
 
 // Retrieve the item values and a function
-export default function ToDo({ item, onUpdate, onDelete }) {  
-    const [isEdit, setIsEdit] = useState(false);
+export default function ToDo({ item, onUpdate, onDelete }) {
+  const [isEdit, setIsEdit] = useState(false);
 
-    // Set isEdit state to true
+  // Set isEdit state to true
+  function handleClick() {
+    setIsEdit(true);
+  }
+
+  // New microComponent
+  function FormEdit() {
+    const [newValue, setNewValue] = useState(item.title); // Set new value
+
+    function handleSubmit(e) {
+      e.preventDefault();
+    }
+
+    // Set the input value as the new state
+    function handleChange(e) {
+      e.preventDefault();
+      const value = e.target.value;
+      setNewValue(value);
+    }
+
+    // Call the function passed as prop onUpdate
     function handleClick() {
-        setIsEdit(true);
-    }
-
-    // New microComponent
-    function FormEdit() {
-        const [newValue, setNewValue] = useState(item.title); // Set new value
-
-        function handleSubmit(e) {
-            e.preventDefault();
-        }
-
-        // Set the input value as the new state
-        function handleChange(e) {
-            e.preventDefault();
-            const value = e.target.value;
-            setNewValue(value);
-        }
-
-        // Call the function passed as prop onUpdate
-        function handleClick() {
-            onUpdate(item.id, newValue);
-            setIsEdit(false);
-        }
-
-        return (
-            <form className="todoUpdate" onSubmit={handleSubmit}>
-                <input type="text" onChange={handleChange} value={newValue}/>
-                <button onClick={handleClick}>Update</button>
-            </form>
-        );
-    }
-
-    function ToDoElement() {
-        return (
-            <div className="toDoInfo">
-                {item.title}
-                <button onClick={handleClick}>Edit</button>
-                <button onClick={(e)=>onDelete(item.id)}>Delete</button>
-            </div>
-        );
+      onUpdate(item.id, newValue);
+      setIsEdit(false);
     }
 
     return (
-        <div className="toDo">
-            {isEdit ? <FormEdit /> : <ToDoElement />}
-        </div>
+      <form className="flex items-center justify-center mb-4" onSubmit={handleSubmit}>
+        <input type="text" className="border border-black p-1 mr-2" onChange={handleChange} value={newValue} />
+        <button className=" bg-green-500 p-2 rounded-md text-white hover:bg-green-800" onClick={handleClick}>Update</button>
+      </form>
     );
+  }
+
+  function ToDoElement() {
+    return (
+      <div className="flex items-center justify-center mb-4">
+        <div className="mr-2 w-48 break-all border p-2">{item.title}</div>
+        <button className="m-2 bg-blue-600 hover:bg-blue-800 p-2 rounded-md text-white" onClick={handleClick}>Edit</button>
+        <button className=" m-2 p-2 bg-red-600 hover:bg-red-900 rounded-md text-white" onClick={() => onDelete(item.id)}>Delete</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-start">
+      {isEdit ? <FormEdit /> : <ToDoElement />}
+    </div>
+  );
 }
--- 001-to-do-list/src/components/todoApp.js
+++ 001-to-do-list/src/components/todoApp.js
@@ -1,55 +1,64 @@
-import { useState } from "react"
+import { useState } from "react";
 import ToDo from "./toDo";
+
 export default function TodoApp() {
-    const [title, setTitle] = useState('');
-    const [toDo, setToDo] = useState([]);
+  const [title, setTitle] = useState("");
+  const [toDo, setToDo] = useState([]);
 
-    function handleChange(e) {
+  function handleChange(e) {
+    e.preventDefault();
+    setTitle(e.target.value);
+  }
 
-        e.preventDefault();
-        setTitle(e.target.value);
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newToDo = {
+      id: crypto.randomUUID(),
+      title: title,
+      completed: false
+    };
+    setToDo([...toDo, newToDo]);
+    setTitle("");
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        const newToDo = {
-            id: crypto.randomUUID(),
-            title: title,
-            completed: false
-        }
-        setToDo([...toDo, newToDo]);  //copy from existing todo list to avoid duplicates in future and add new todo list
-        setTitle("");
-    }
+  function handleUpdate(id, value) {
+    const temp = [...toDo];
+    const item = temp.find((item) => item.id === id);
+    item.title = value;
+    setToDo(temp);
+  }
 
-    function handleUpdate (id,value){
-        const temp = [...toDo] // copy array
-        const item =  temp.find(item => item.id === id); //find the item with the given id
-        item.title = value; //save the value
-        setToDo(temp) //set the new array to the current one and update the existing one with the new value
-    }
+  function handleDelete(id) {
+    const temp = toDo.filter((item) => item.id !== id);
+    setToDo(temp);
+  }
 
-    function handleDelete (id) {
-        const temp = toDo.filter(item => item.id !== id)
-        setToDo(temp);
-    }
-    return (
-        <div className="todoContainer">
-            <form action="">
-                <input className="todoInput" onChange={handleChange} value={title}/>
-                <input
-                    onClick={handleSubmit}
-                    className="buttonCreate"
-                    type="submit"
-                    value="Create To Do" />
-            </form>
+  return (
+    <div className="flex flex-col items-center justify-start mt-4">
+      <form action="" className="mb-4">
+        <input
+          className="border border-black p-1"
+          onChange={handleChange}
+          value={title}
+        />
+        <input
+          onClick={handleSubmit}
+          className="m-2 border  p-2 rounded-md bg-gray-300 hover:bg-gray-500"
+          type="submit"
+          value="Create To Do"
+        />
+      </form>
 
-            <div className="todDoContainer">
-                {
-                    toDo.map(item => (
-                       <ToDo key={item.id} item={item} onUpdate={handleUpdate} onDelete={handleDelete}/>
-                    ))
-                }
-            </div>
-        </div>
-    );
+      <div className="">
+        {toDo.map((item) => (
+          <ToDo
+            key={item.id}
+            item={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
