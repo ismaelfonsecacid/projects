ab9ecf6c25568c6042cee71f93d1fae03b8bf091
Ismael <isma@isma.es>
2023-07-04 10:33:13 +0200

    Weather App without CSS full function

---
+++ 002-weather-app/src/components/Weather.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import WeatherForm from './weatherForm'
+import WeatherMainInfo from './WeatherMainInfo';
+
+export default function Weather() {
+
+    const [weather, setWeather] = useState(null)
+
+    useEffect(() => {   //se ejecuta cada vez que sucede un renderizado o cuando se destruye
+        loadInfo();
+    }, [])
+
+    useEffect(() => {
+        document.title = `Weather | ${weather?.location.name ?? ''}`
+    }, [weather])
+
+    async function loadInfo(city = 'london') {
+        try {
+            const request = await fetch(
+                `https://api.weatherapi.com/v1/current.json?key=ed33efce0a564c2e8a674053230407&q=${city}&aqi=no`
+            );
+
+            const json = await request.json()
+            setWeather(json);
+        } catch (error) {
+
+        }
+    }
+    function handleChangeCity(city) {
+        setWeather(null)
+        loadInfo(city);
+    }
+
+
+    return (
+        <>
+            <div className='bg-red-700'>
+                <WeatherForm onChangeCity={handleChangeCity}/>
+            </div>
+            <div>
+                <WeatherMainInfo weather={weather}/>
+            </div>
+        </>
+    )
+}
---
+++ 002-weather-app/src/components/WeatherMainInfo.jsx
@@ -0,0 +1,35 @@
+
+
+
+export default function WeatherMainInfo({ weather }) {
+
+
+    return (
+        <div>
+            <div>
+                {weather?.location.name}
+            </div>
+            <div>
+                {weather?.location.country}
+            </div>
+            <div>
+                <img src={`http:${weather?.current.condition.icon}`} width='128' alt="imagen" />
+            </div>
+            <div>
+                <div>
+                    {weather?.current.condition.text}
+                </div>
+                <div>
+                    {weather?.current.temp_c}º
+                </div>
+            </div>
+            <iframe
+                title="mapa"
+                src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15057.534307180755!2d${weather?.location.lon}5!3d${weather?.location.lat}5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smx!4v1651103744472!5m2!1sen!2smx`}
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                loading="lazy"></iframe>
+        </div>
+    )
+};
