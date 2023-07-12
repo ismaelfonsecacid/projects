d3f292a0192a422820b8a1d4620bbd3e4052a951
Ismael <isma@isma.es>
2023-07-12 13:01:06 +0200

    Game

---
+++ 003-linkedin-learning/009-rock-paper-scissors/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ROCK - PAPER - SCISSORS</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
