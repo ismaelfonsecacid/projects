c6068e2ab63387851178823a3bdf942bdaf3107b
Ismael <ismanoallaps4@gmail.com>
2023-08-11 09:50:23 +0200

    connect expressJS

---
+++ MERN/server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {} = require('./constants');
+
+const app = express();
+
+//Import routes
+
+//Configure Body Parse
+
+//Configure Header HTTP - CORS
+
+//Configure routings
+
+module.exports = app;
--- MERN/server/index.js
+++ MERN/server/index.js
@@ -1,11 +1,20 @@
 const mongoose  = require('mongoose');
 const {DB_USER,DB_PASSWORD,DB_HOST,API_VERSION,IP_SERVER} = require('./constants');
+const app = require('./app');
+
+
+const PORT = process.env.POST || 3977;
 
 const connectDB = async () => {
     try {
       await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`)
   
-      console.log('La conexión con la base de datos ha sido exitosa.');
+      app.listen(PORT, () => {
+        console.log('#################################')
+        console.log('############ API REST ###########')
+        console.log('#################################')
+        console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}/`)
+      })
     } catch (err) {
       console.log('Error al conectar a la base de datos', err);
     }
