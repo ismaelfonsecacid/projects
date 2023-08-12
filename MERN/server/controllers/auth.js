94c679e3ee69dc9734a68279b8f61b30977ca8d1
Ismael <ismanoallaps4@gmail.com>
2023-08-12 23:09:10 +0200

    Add jsonwetoken and bcrypt

--- MERN/server/controllers/auth.js
+++ MERN/server/controllers/auth.js
@@ -1,9 +1,32 @@
-function register(req,res) {
-console.log('Se ha ejecutado correctamente')
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
-res.status(200).send({ msg: "TODO OK"});
+async function register(req, res) {
+    const { firstname, lastname, email, password } = req.body;
 
+    if (!email) return res.status(400).send({ msg: "El email es obligatorio" });
+    if (!password) return res.status(400).send({ msg: "La password es obligatoria" });
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({
+            firstname,
+            lastname,
+            email: email.toLowerCase(),
+            role: "user",
+            active: false,
+            password: hashPassword
+        });
+
+        const userStorage = await user.save();
+        res.status(200).send(userStorage);
+    } catch (error) {
+        res.status(400).send({ msg: "Error al crear el usuario" });
+    }
 }
+
 module.exports = {
     register,
-}
+};
