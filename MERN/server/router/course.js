3e902602e0f8243ee4f19f2f6c06edc42c989cdc
Ismael <ismanoallaps4@gmail.com>
2023-08-16 16:14:55 +0200

    create and getCourses

---
+++ MERN/server/router/course.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const CourseController = require('../controllers/course')
+const multiparty = require('connect-multiparty'); //this is for images
+
+const md_auth = require('../middleware/authenticated');
+const md_upload = multiparty({
+    uploadDir: "./uploads/course"
+})
+
+const api = express.Router()
+
+api.post("/course", [md_auth.asureAuth, md_upload], CourseController.createCourse)
+api.get("/course", CourseController.getCourses)
+
+
+module.exports = api;
