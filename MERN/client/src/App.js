58a4587d055484be1f7565d3c879ca513ea7aa15
Ismael <ismanoallaps4@gmail.com>
2023-08-17 09:53:50 +0200

    add lodash

--- MERN/client/src/App.js
+++ MERN/client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+
 
 export default function App() {
   return (
-    <div>
-      <h1>Estamos</h1>
-    </div>
+
+    <Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label='I agree to the Terms and Conditions' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+
   )
 }
