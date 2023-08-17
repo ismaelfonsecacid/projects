f07088f681877d2314ed8ade2719eced68c94960
Ismael <ismanoallaps4@gmail.com>
2023-08-17 23:04:14 +0200

    add Users and control about the session

--- MERN/client/src/router/AdminRouter.js
+++ MERN/client/src/router/AdminRouter.js
@@ -1,20 +1,32 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import {Auth} from '../pages/admin'
-import {AdminLayout} from "../layouts"
+import { Routes, Route } from 'react-router-dom'
+import { Auth } from '../pages/admin/Auth/Auth'
+import { Users } from '../pages/admin/Users/Users'
+import { AdminLayout } from "../layouts"
+
+const user = null;
 
 export default function WebRouter() {
 
-  const loadLayout = (Layout,Page) => {
+  const loadLayout = (Layout, Page) => {
     return (
       <Layout>
-        <Page/>
+        <Page />
       </Layout>
     )
   }
   return (
     <Routes>
-        <Route path='/admin/*' element={loadLayout(AdminLayout,Auth)}/>
+      {!user ? (
+
+        <Route path='/admin/*' element={loadLayout(AdminLayout, Auth)} />
+
+      ) : (
+        <>
+          <Route path='/admin/users*' element={loadLayout(AdminLayout, Users)} />
+
+        </>
+      )}
     </Routes>
   )
 }
