5874a5253b3205bc77e049f4594f22161dcceb76
Ismael <ismanoallaps4@gmail.com>
2023-08-17 23:47:33 +0200

    index.js

--- MERN/client/src/pages/web/index.js
+++ MERN/client/src/pages/web/index.js
@@ -1,1 +1,7 @@
-export * from './Home'
+// src/pages/web/index.js
+export { default as Blog } from './Blog/Blog';
+export { default as Contact } from './Contact/Contact';
+export { default as Courses } from './Courses/Courses';
+export { default as Post } from './Post/Post';
+export { default as Home } from './Home/Home';
+
