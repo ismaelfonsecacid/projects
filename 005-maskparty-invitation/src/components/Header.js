3e4739d7c169e719dd074c7ccd9d19f0722e1871
Ismael <ismanoallaps4@gmail.com>
2023-08-20 15:35:13 +0200

    reload

--- 005-maskparty-invitation/src/components/Header.js
+++ 005-maskparty-invitation/src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
             <header className="header">
                 <div><img src='/img/mask2.png' style={{ width: '200px', marginLeft: '5%' }} /></div>
                 <h1 className="golden-title">Fiesta de máscaras</h1>
-                <h1 className="golden-title">Felices 20</h1>
+                <h1 className="letter-title">Felices Veinte</h1>
             </header>
         </>
 
