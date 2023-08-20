8b05fee34b9115ad4bbb502dac6b08b7407487b9
Ismael <ismanoallaps4@gmail.com>
2023-08-20 13:03:58 +0200

    random

--- 005-maskparty-invitation/src/App.js
+++ 005-maskparty-invitation/src/App.js
@@ -1,16 +1,31 @@
 // src/App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import InvitationDetails from './components/InvitationDetails';
 import './App.css';
 
 function App() {
+  const [showHeader, setShowHeader] = useState(true);
+
+  useEffect(() => {
+    // Mostrar el componente Header durante 4 segundos y luego cambiar a InvitationDetails
+    const timer = setTimeout(() => {
+      setShowHeader(false);
+    }, 4000);
+
+    return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta antes de que termine
+
+  }, []);
+
   return (
     <div className="App">
-      <Header />
-      <div className='marco'>
-        <InvitationDetails />
-      </div>
+      {showHeader ? (
+        <Header />
+      ) : (
+        <div className='marco'>
+          <InvitationDetails />
+        </div>
+      )}
     </div>
   );
 }
