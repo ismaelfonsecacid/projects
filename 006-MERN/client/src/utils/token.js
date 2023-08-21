3edfc4e2bdccd2facee8dd40990a374deb74bb2e
Ismael <ismanoallaps4@gmail.com>
2023-08-21 11:41:03 +0200

    function check if token has expired

---
+++ 006-MERN/client/src/utils/token.js
@@ -0,0 +1,13 @@
+import jwtDecode from 'jwt-decode'
+
+export const hasExpiredToken = (token) => {
+    const { exp } = jwtDecode(token);
+    const currentData = new Date().getTime();
+
+    if (exp <= currentData) {
+        return true;
+    }
+
+    return false;
+
+}
