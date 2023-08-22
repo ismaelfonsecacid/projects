abbccc3026ec024153a1aef44ecf3bc69d1142e1
Ismael <ismanoallaps4@gmail.com>
2023-08-22 09:24:37 +0200

    AdminLayout styles

--- 006-MERN/client/src/components/Admin/index.js
+++ 006-MERN/client/src/components/Admin/index.js
@@ -1,7 +1,7 @@
 // src/components/Admin/index.js
 import { RegisterForm } from './Auth/RegisterForm';
 import { LoginForm } from './Auth/LoginForm';
-
+import { AdminMenu } from './AdminLayout/AdminMenu'
 export { RegisterForm };
-
+export { AdminMenu };
 export { LoginForm };
--- 006-MERN/client/src/layouts/AdminLayout/AdminLayout.js
+++ 006-MERN/client/src/layouts/AdminLayout/AdminLayout.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { AdminMenu } from '../../components/Admin'
+
+import { Icon } from '../../assets'
 import './AdminLayout.scss'
 export function AdminLayout(props) {
   const { children } = props
   return (
     <div className='admin-layout'>
       <div className='admin-layout__left'>
-        <div className='logo'>LOGO</div>
-        <span>admin menu</span>
+        <Icon.LogoWhite className='logo' />
+        <AdminMenu />
       </div>
       <div className='admin-layout__right'>
         <div className='admin-layout__right-header'>
