d920b85f30d6b78bdbdf4cff3228f058cfd42971
Ismael <ismanoallaps4@gmail.com>
2023-08-30 10:51:27 +0200

    update changes

--- 006-MERN/client/src/api/user.js
+++ 006-MERN/client/src/api/user.js
@@ -1,26 +1,57 @@
-import { ENV } from '../utils'
+import { ENV } from "../utils";
 
 class User {
-    baseApi = ENV.BASE_API;
+	baseApi = ENV.BASE_API;
 
-    async getMe(accessToken) {
-        try {
-            const url = `${this.baseApi}/${ENV.API_ROUTES.USER_ME}`;
-            const params = {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            }
-            const response = await fetch(url, params)
-            const result = await response.json();
+	async getMe(accessToken) {
+		try {
+			const url = `${this.baseApi}/${ENV.API_ROUTES.USER_ME}`;
+			const params = {
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			};
+			const response = await fetch(url, params);
+			const result = await response.json();
 
-            if (response.status !== 200) throw result;
+			if (response.status !== 200) throw result;
 
-            return result;
-        } catch (error) {
-            throw error;
-        }
-    }
+			return result;
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async createUser(accessToken, data) {
+		try {
+			const formData = new FormData(); //use for frop img
+			Object.keys(data).forEach((key) => {
+				formData.append(key, data[key]);
+			});
+
+			if (data.fileAvatar) {
+				formData.append("avatar", data.fileAvatar);
+			}
+
+			const url = `${this.baseApi}/${ENV.API_ROUTES.USER}`;
+			const params = {
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+				body: formData,
+			};
+
+			const response = await fetch(url, params);
+			const result = await response.json();
+
+			if (response.status !== 201) throw result;
+
+			return result;
+		} catch (error) {
+			throw error;
+		}
+	}
 }
 
 export default User;
