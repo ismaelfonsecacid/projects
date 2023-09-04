5cadd7189c829cb61107b8cd8be8f9d37864a49b
Ismael <ismanoallaps4@gmail.com>
2023-09-04 12:04:03 +0200

    blog layout

--- 006-MERN/client/src/pages/admin/Blog/Blog.js
+++ 006-MERN/client/src/pages/admin/Blog/Blog.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Tab, Button } from "semantic-ui-react";
+import { ListPost, PostForm } from "../../../components/Admin";
+import { BasicModal } from "../../../components/Shared";
+import "./Blog.scss";
 
 export default function Blog() {
-    return (
-        <div>
-            <h1>Estamos en el blog</h1>
-        </div>
-    )
+	const [showModal, setShowModal] = useState(false);
+	const [reload, setReload] = useState(false);
+
+	const onOpenCloseModal = () => setShowModal((prevState) => !prevState);
+	const onReload = () => setReload((prevState) => !prevState);
+	const panes = [
+		{
+			render: () => (
+				<Tab.Pane attached={false}>
+					<ListPost reload={reload} onReload={onReload} />
+				</Tab.Pane>
+			),
+		},
+	];
+	return (
+		<>
+			<div className="blog-page">
+				<div className="blog-page__add">
+					<Button primary onClick={onOpenCloseModal}>
+						Nuevo Post
+					</Button>
+				</div>
+				<Tab menu={{ secondary: true }} panes={panes} />
+			</div>
+
+			<BasicModal
+				show={showModal}
+				close={onOpenCloseModal}
+				title="Crear nuevo post"
+				size="large"
+			>
+				<PostForm onClose={onOpenCloseModal} onReload={onReload} />
+			</BasicModal>
+		</>
+	);
 }
