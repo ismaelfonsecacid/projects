74545534223f1a35b370e2b8500b0f3e817d3f58
Ismael <ismanoallaps4@gmail.com>
2023-09-05 21:48:11 +0200

    webrouter

--- 006-MERN/client/src/router/WebRouter.js
+++ 006-MERN/client/src/router/WebRouter.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { ClientLayout } from '../layouts'
-import { Blog, Post, Contact, Courses, Home } from '../pages/web'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ClientLayout } from "../layouts";
+import { Blog, Post, Contact, Courses, Home } from "../pages/web";
 
 export default function WebRouter() {
-
-  const loadLayout = (Layout, Page) => {
-    return (
-      <Layout>
-        <Page />
-      </Layout>
-    )
-  }
-  return (
-    <Routes>
-      <Route path='/' element={loadLayout(ClientLayout, Home)} />
-      <Route path='/courses' element={loadLayout(ClientLayout, Courses)} />
-      <Route path='/contact' element={loadLayout(ClientLayout, Contact)} />
-      <Route path='/blog' element={loadLayout(ClientLayout, Blog)} />
-      <Route path='/blog/:path' element={loadLayout(ClientLayout, Post)} />
-    </Routes>
-  )
+	const loadLayout = (Layout, Page) => {
+		return (
+			<Layout>
+				<Page />
+			</Layout>
+		);
+	};
+	return (
+		<Routes>
+			<Route path="/" element={loadLayout(ClientLayout, Home)} />
+			<Route path="/courses" element={loadLayout(ClientLayout, Courses)} />
+			<Route path="/blog" element={loadLayout(ClientLayout, Blog)} />
+			<Route path="/blog/:path" element={loadLayout(ClientLayout, Post)} />
+		</Routes>
+	);
 }
