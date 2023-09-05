2c23a4c8a73ee5b215d35f21d6229d19db16c438
Ismael <ismanoallaps4@gmail.com>
2023-09-05 10:02:13 +0200

    index

---
+++ 006-MERN/client/src/components/Web/index.js
@@ -0,0 +1,6 @@
+import { TopBar } from "./TopBar";
+import { Info } from "./Footer/Info";
+import { Menu } from "./Footer/Menu";
+import { Newsletter } from "./Footer/Newsletter";
+
+export { TopBar, Info, Menu, Newsletter };
